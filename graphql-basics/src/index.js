804396a5c44e6c82ac9c8184c3bf97264da01bfb
Wako <dulo.rodrigo@gmail.com>
2019-08-12 22:38:24 -0500

    Pasing data to the queries

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -4,7 +4,10 @@ const { GraphQLServer } = require('graphql-yoga')
 
 const typeDefs = `
   type Query {
+    greeting(name: String, position: String): String!
+    sum(a: Float!, b: Float!): Float!
     me: User!
+    post: Post!
   }
   type User {
     id: ID!
@@ -12,17 +15,37 @@ const typeDefs = `
     email: String!
     age: Int
   }
+  type Post {
+    id: ID!
+    tittle: String!
+    body: String!
+    published: Boolean!
+  }
 `
 
 // Resolvers
 const resolvers = {
   Query: {
+    greeting (parent, args) {
+      return `Hello ${args.name}. You are my favorite ${args.position}`
+    },
+    sum (parent, args) {
+      return args.a + args.b
+    },
     me () {
       return {
         id: '666ddd',
         name: 'Wako',
         email: 'wako@wako.com',
         age: null
+      }
+    },
+    post () {
+      return {
+        id: '9992ss',
+        tittle: 'My first post',
+        body: 'Lorem Ipsum',
+        published: false
       }
     }
   }
