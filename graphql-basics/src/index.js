296acf7a0b5592f68a7916867eb9efe493e451e3
Wako <dulo.rodrigo@gmail.com>
2019-08-18 20:19:52 -0500

    Add comments to post

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -56,22 +56,26 @@ const comments = [
   {
     id: '1',
     text: 'First comment!',
-    author: '1'
+    author: '1',
+    post: '2'
   },
   {
     id: '2',
     text: 'Second comment',
-    author: '2'
+    author: '2',
+    post: '2'
   },
   {
     id: '3',
     text: 'Third comment',
-    author: '1'
+    author: '1',
+    post: '2'
   },
   {
     id: '4',
     text: 'Fourth comment',
-    author: '3'
+    author: '3',
+    post: '1'
   }
 ]
 
@@ -95,11 +99,13 @@ const typeDefs = `
     body: String!
     published: Boolean!
     author: User!
+    comments: [Comment!]!
   }
   type Comment {
     id: ID!
     text: String!
     author: User!
+    post: Post!
   }
 `
 
@@ -119,6 +125,9 @@ const resolvers = {
   Post: {
     author (parent, args, ctx, info) {
       return users.find(user => user.id === parent.author)
+    },
+    comments (parent, args, ctx, info) {
+      return comments.filter(comment => comment.post === parent.id)
     }
   },
   User: {
@@ -132,6 +141,9 @@ const resolvers = {
   Comment: {
     author (parent, args, ctx, info) {
       return users.find(user => user.id === parent.author)
+    },
+    post (parent, args, ctx, info) {
+      return posts.find(post => post.id === parent.post)
     }
   }
 }
