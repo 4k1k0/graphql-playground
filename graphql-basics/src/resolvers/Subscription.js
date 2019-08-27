24a2dc8745ae1868ab7d23b93e0048772fee175e
Wako <dulo.rodrigo@gmail.com>
2019-08-27 15:27:48 -0500

    new subscripttion

--- graphql-basics/src/resolvers/Subscription.js
+++ graphql-basics/src/resolvers/Subscription.js
@@ -12,6 +12,14 @@ const Subscription = {
       }, 1000)
       return pubsub.asyncIterator('count')
     }
+  },
+  comment: {
+    subscribe (parent, args, { db, pubsub }, info) {
+      const { postId } = args
+      const post = db.posts.find(p => p.id === postId && p.published)
+      if (!post) throw new Error('Post not found')
+      return pubsub.asyncIterator(`comment ${postId}`)
+    }
   }
 }
 
